5f110fb5f5f1ff4d36b5c11b942f760de2f63fcd
svetlana_korneeva <svet-ko@list.ru>
2022-11-20 14:24:23 +0200

    feat: different jwt keys for prod and develop

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -67,7 +67,8 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const jwtSecretKey = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
+      const token = jwt.sign({ _id: user._id }, jwtSecretKey, { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((e) => {
